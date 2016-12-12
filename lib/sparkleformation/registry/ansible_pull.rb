eaf9760dbbfaa9b694efbda234e892c5d988b248
Greg Swallow <gswallow@gmail.com>
2016-12-12 16:00:35 -0500

    Initial commit

---
+++ lib/sparkleformation/registry/ansible_pull.rb
@@ -0,0 +1,48 @@
+SfnRegistry.register(:ansible_pull) do |_name, _config = {}|
+
+  # Yeah, only Ubuntu is supported.  Sorry.
+  # Ansible doesn't recommend using an omnibus install
+  # method so I'm stuck with either pip, or packages.
+
+  seed = Array.new
+  _config.fetch(:ansible_seed, {}).each do |k, v|
+    seed << "#{k.upcase}="
+    seed << v
+    seed << "\n"
+  end
+
+  version = Array.new
+  if _config.has_key?(:ansible_version)
+    version = [ " =", _config[:ansible_version], "~`lsb_release -s -c`" ]
+  end
+
+  metadata('AWS::CloudFormation::Init') do
+    _camel_keys_set(:auto_disable)
+    configSets do |sets|
+      sets.default += ['ansible']
+    end
+    ansible do
+      files('/etc/ansible-local/seed') do
+        content join!(seed)
+        mode '000600'
+        owner 'root'
+        group 'root'
+      end
+      commands('00-add-ansible-ppa') do
+        command 'apt-get -q update && apt-get -yqq install software-properties-common && apt-add-repository ppa:ansible/ansible && apt-get -q update'
+        test 'test ! -e ansible-ubuntu-ansible-xenial.list'
+      end
+      commands('01-install-ansible') do
+        command join!('apt-get -yqq install ansible', *version)
+      end
+      commands('02-run-ansible-pull') do
+        command join!('env $(cat /etc/ansible-local/seed) ansible-pull -U',
+                      _config[:ansible_playbook_repo],
+                      '-d /etc/ansible-local -C',
+                      _config.fetch(:ansible_playbook_branch, 'master'),
+                      _config.fetch(:ansible_local_yaml_path, 'local.yml'),
+                      {:options => { :delimiter => ' '}})
+      end
+    end
+  end
+end
---
+++ sparkle-pack-cfn-init-ansible.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name = 'sparkle-pack-cfn-init-ansible'
+  s.version = '0.0.1'
+  s.licenses = ['MIT']
+  s.summary = 'CFN Init Ansible SparklePack'
+  s.description = 'SparklePack to provide cfn-init config set to bootstrap an instance using Ansible'
+  s.authors = ['Greg Swallow']
+  s.email = 'gswallow@gmail.com'
+  s.homepage = 'https://github.com/gswallow'
+  s.files = Dir[ 'lib/sparkleformation/registry/*' ] + %w(sparkle-pack-cfn-init-ansible.gemspec lib/sparkle-pack-cfn-init-ansible.rb)
+  s.add_runtime_dependency 'sparkle_formation'
+end
