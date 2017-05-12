c1fb789efb3ad35a754fe83e0ff7cc770c44cfb4
Greg Swallow <gswallow@gmail.com>
2017-05-11 21:21:34 -0400

    Test for cfn helper scripts; install if missing.

--- lib/sparkleformation/registry/user_data.rb
+++ lib/sparkleformation/registry/user_data.rb
@@ -26,6 +26,20 @@ SfnRegistry.register(:user_data) do |_name, _config={}|
       "  exit $status\n",
       "}\n\n",
 
+      "if [ ! -f /usr/local/bin/cfn-init ]; then\n",
+      "  apt-get -qq update\n",
+      "  apt-get -yqq install python-setuptools python-pip python-lockfile unzip\n",
+      "  apt-get -yqq install --reinstall ca-certificates\n",
+      "  easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz\n",
+      "fi\n\n",
+
+      "if [ ! -f /usr/local/bin/aws ]; then\n",
+      "  curl -sSL https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o /tmp/awscli-bundle.zip\n",
+      "  unzip -q -d /tmp /tmp/awscli-bundle.zip\n",
+      "  /tmp/awscli-bundle/install -i /opt/aws -b /usr/local/bin/aws\n",
+      "  rm -rf /tmp/awscli-bundle*\n",
+      "fi\n\n",
+
       "/usr/local/bin/cfn-init -s ", stack_name!, " --resource ", _config[:launch_config],
       "  --region ", region!, " || cfn_signal_and_exit\n\n",
 
